60a380d6de8974ff7f200158662a22a43e45aec4
Rafael Garcia <rafbgarcia@gmail.com>
2018-09-03 00:41:55 -0300

    Nanobox

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,9 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :states_api, StatesApiWeb.Endpoint,
-  http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "example.com", port: 80],
+  http: [port: 8080],
+  # http: [:inet6, port: System.get_env("PORT") || 4000],
+  url: [host: "enderecify.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
